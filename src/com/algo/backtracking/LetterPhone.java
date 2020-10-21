98804521fdc512d9a4c223dc66f579ab82d90c17
rajivnitb <44631447+rajivnitb@users.noreply.github.com>
2020-10-21 19:10:58 +0530

    Add files via upload

---
+++ src/com/algo/backtracking/LetterPhone.java
@@ -0,0 +1,50 @@
+package com.algo.backtracking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class LetterPhone {
+
+	static Map<Integer, String> map = new HashMap<Integer, String>();
+	static ArrayList<String> list = new ArrayList<String>();
+
+	public ArrayList<String> letterCombinations(int[] a, String current, int index) {
+
+		if (index > a.length - 1) {
+			list.add(current);
+			return list;
+		}
+
+		String s = map.get(a[index]);
+		for (int i = 0; i <= s.length() - 1; i++) {
+			if (current == null) {
+				current = new String();
+			}
+			current += s.charAt(i);
+			letterCombinations(a, current, index + 1);
+            current=current.substring(0,index) ;
+           
+		}
+
+		return list;
+
+	}
+
+	public static void main(String args[]) {
+
+		map.put(1, null);
+		map.put(2, "abc");
+		map.put(3, "def");
+		map.put(4, "ghi");
+		map.put(5, "jkl");
+		map.put(6, "mno");
+		map.put(7, "pqrs");
+		map.put(8, "tuv");
+		map.put(9, "wxyz");
+		int[] a = new int[] { 2, 3, 4 };
+		new LetterPhone().letterCombinations(a,null, 0);
+		System.out.println(list);
+	}
+}
---
+++ src/com/algo/backtracking/SubSet.java
@@ -0,0 +1,38 @@
+package com.algo.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSet {
+
+	static List<Integer> ls = new ArrayList<Integer>();
+
+	public void subsets(int[] a, int index, int nextIndex) {
+
+		if (nextIndex == a.length && index <= a.length) {
+			subsets(a, index + 1, index + 1);
+		}
+
+		if (nextIndex > a.length - 1 || index > a.length - 1) {
+			return;
+		}
+
+		if (index == nextIndex) {
+			ls.add(a[index]);
+		}
+
+		if (index != nextIndex) {
+			String s = new String();
+			s = String.valueOf(ls.get(ls.size() - 1)) + String.valueOf(a[nextIndex]);
+			ls.add(Integer.valueOf(s));
+		}
+
+		subsets(a, index, nextIndex + 1);
+		
+	}
+
+	public static void main(String args[]) {
+		new SubSet().subsets(new int[] { 1, 2, 3 }, 0, 0);
+		System.out.println(ls);
+	}
+}
