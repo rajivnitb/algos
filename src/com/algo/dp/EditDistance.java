970a5c0dc332ff7ee03b90d14987f489dedbdd74
rajivnitb <44631447+rajivnitb@users.noreply.github.com>
2020-12-25 18:12:08 +0530

    Update EditDistance.java

--- src/com/algo/dp/EditDistance.java
+++ src/com/algo/dp/EditDistance.java
@@ -2,40 +2,38 @@ package com.algo.dp;
 
 public class EditDistance {
 	
-	private int min(int x,int y,int z) {
-		  if (x <= y && x <= z) 
-	            return x; 
-	        if (y <= x && y <= z) 
-	            return y; 
-	        else
-	            return z; 
-	}
+	public int getDistance(String s1,String s2) {
+		
+		if (s1.length() <= 0 ) {
+			return s2.length();
+		}
+		
+		if (s2.length() <= 0 ) {
+			return s1.length();
+		}
+		
+		
+		if (s1.charAt(0) == s2.charAt(0)) {
+			return getDistance(s1.substring(1),s2.substring(1));
+		}
+		
+		
+		
+		
+		return Math.min(Math.min(getDistance(s1,s2.substring(1))+1
+				,getDistance(s1.substring(1),s2)+1),
+				getDistance(s1.substring(1),s2.substring(1)+1));
+		
+		
+		
+		
+	}	
 	
-	private int getDistance(String str1,String str2,int n,int m) {
-		
-		if (n == 0) {
-			return m;
-		}
-		
-		if (m == 0) {
-			return n;
-		}
-		
-		if (str1.charAt(n-1) == str2.charAt(m-1)) {
-			return getDistance(str1,str2,n-1,m-1);
-		}
-		
-		return 1+min(getDistance(str1,str2,n,m-1),getDistance(str1,str2,n-1,m),getDistance(str1,str2,n,m));
-		
-		
-	}
-	
-	public static void main(String args[]) {
-		String str1 = "Anshuman";
-		String str2 = "Antihuman";
-		
-		System.out.println(new EditDistance().getDistance(str1, str2,str1.length(),str2.length()));
-		
+	public static void main(String [] args) {
+		String s1 = "sunday";
+		String s2 = "saturday";
+	  
+		System.out.println(new EditDistance().getDistance(s1, s2));
 		
 	}
 }
